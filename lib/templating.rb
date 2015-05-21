e41f249c47ba35414ed5c1916438412d472c9c8b
David Ker <snappy316@gmail.com>
2015-05-21 15:29:08 -0700

    DRY up a bit, and require a couple values

--- lib/templating.rb
+++ lib/templating.rb
@@ -1,24 +1,19 @@
 #!/usr/bin/env ruby
 require 'yaml'
 
-print "What is the customer's first name? - "
-first_name = gets.chomp
+def get_value(question)
+  print "#{question} - "
+  value = gets.chomp
+  return value unless value.nil?
+end
 
-print "What is the customer's last name? - "
-last_name = gets.chomp
-
-puts "What should the salutation be?"
-puts "Use '@first_name' for their first name,"
-print "and '@last_name' for their last name - "
-salutation = gets.chomp
-
-print "What product is the customer interested in? - "
-product = gets.chomp
-
-address = "1 Infinite Loop\nSomewheresville, CA 98765"
-
-salutation.gsub!("@first_name", first_name)
-salutation.gsub!("@last_name", last_name)
+def get_required_value(question)
+  value = ""
+  loop do
+    value = get_value(question)
+    return value unless value.nil? || value.length < 1
+  end
+end
 
 def database
   File.dirname(File.absolute_path(__FILE__)) + '/body.txt'
@@ -27,6 +22,20 @@ end
 def load
   read_file = File.read(database)
 end
+
+first_name = get_required_value("What is the customer's first name?")
+last_name = get_value("What is the customer's last name?")
+salutation = get_value("What should the salutation be? (Use '@first_name' and '@last_name')")
+product = get_required_value("What product is the customer interested in?")
+address = "1 Infinite Loop\nSomewheresville, CA 98765"
+
+if salutation.length > 0
+  salutation.gsub!("@first_name", first_name)
+  salutation.gsub!("@last_name", last_name)
+else
+  salutation = "Dear #{first_name}"
+end
+
 
 email_body = load
 
